446f59cd999c75088381836728fb82d456db60e4
Jüri Vaitmaa <jyri.vaitmaa@tthk.ee>
2024-10-17 11:58:57 +0300

    Finished index view, working controller, to make said view work
    Titans, now has an index page, which currently is treated as the inventory for the player.

---
+++ GalacticTitans/Controllers/TitansController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GalacticTitans.Controllers
+{
+    public class TitansController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
