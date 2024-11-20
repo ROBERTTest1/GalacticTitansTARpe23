342934c48fac214e3b12a6d23118414b980b97d2
Estlib <75094683+Estlib@users.noreply.github.com>
2024-11-20 15:04:50 +0200

    Added methods for addpassword view, added the view itself aswell

---
+++ GalacticTitans/Controllers/AccountsController.cs
@@ -0,0 +1,64 @@
+﻿using GalacticTitans.Core.Domain;
+using GalacticTitans.Core.Dto.AccountsDtos;
+using GalacticTitans.Data;
+using GalacticTitans.Models.Accounts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GalacticTitans.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly GalacticTitansContext _context;
+
+        public AccountsController
+            (UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            GalacticTitansContext context
+            )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddPassword()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var userHasPassword = await _userManager.HasPasswordAsync(user);
+            if ( userHasPassword )
+            {
+                RedirectToAction("ChangePassword");
+            }
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddPassword(AddPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                var result = await _userManager.AddPasswordAsync(user, model.NewPassword);
+                if (!result.Succeeded) 
+                { 
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+                await _signInManager.RefreshSignInAsync(user);
+                return View("AddPasswordConfirmation");  
+            }
+            return View(model);
+        }
+
+    }
+
+    
+}
