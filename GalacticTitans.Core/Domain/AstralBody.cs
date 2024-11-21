c03a29c3f7a7fe0ca132e87b207b75dd3949ac26
Estlib <75094683+Estlib@users.noreply.github.com>
2024-11-21 17:24:19 +0200

    Added model for planets "AstralBody", added 2 new area links, added 1 new error
    planet model is able to account for other types of astral bodies aswell, like stars, black holes, moons, etc. the new added error is currently unused, but is prepared for when the user account system is completed in future.

---
+++ GalacticTitans.Core/Domain/AstralBody.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticTitans.Core.Domain
+{
+    public enum AstralBodyType
+    {
+        BlackHole_SuperMassive,
+        BlackHole_Common,
+        Blackhole_Small,
+        Star_RedGiant,
+        Star_Common,
+        Star_WhiteDwarf,
+        Star_Neutron,
+        Star_RedDwarf,
+        Star_BrownDwarf,
+        DebrisCloud,
+        DysonSphere,
+        Planet_GasGiant,
+        Planet_IceGiant,
+        Planet_RockGiant,
+        Planet_LargeGasIce,
+        Planet_Gas,
+        Planet_GasRock,
+        Planet_MegaTerrestrial,
+        Planet_Terrestrial,
+        Planet_Dwarf,
+        Moon_Giant,
+        Moon_Lunar,
+        Moon_GravLockObj,
+        Asteroid,
+        Meteor,
+        Comet,
+        Satellite
+    }
+    public enum KardashevScale
+    {
+        Type1,Type2,Type3
+    }
+    public class AstralBody
+    {
+        public Guid ID { get; set; }
+        public string AstralBodyName { get; set; }
+        public AstralBodyType AstralBodyType { get; set; }
+        public TitanType EnvironmentBoost { get; set; }
+        public string AstralBodyDescription { get; set; }
+        public int MajorSettlements { get; set; }
+        public KardashevScale TechnicalLevel { get; set; }
+        //public List<Titan> TitansWhoOwnThisPlanet { get; set; }
+        public Titan TitanWhoOwnsThisPlanet { get; set; }
+        //public Guid PlayerProfileID { get; set; }
+        public Guid  SolarSystemID { get; set; }
+
+        //db
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
