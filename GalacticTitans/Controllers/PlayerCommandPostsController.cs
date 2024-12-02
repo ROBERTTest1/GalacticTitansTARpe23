4f5ed774ca859a12df16615453ab005fdc579a00
Estlib <75094683+Estlib@users.noreply.github.com>
2024-12-02 14:55:41 +0200

    playerside view layout prototype
    if you remember mentions of bitefight, then this should look vaguely familiar

---
+++ GalacticTitans/Controllers/PlayerCommandPostsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GalacticTitans.Controllers
+{
+    public class PlayerCommandPostsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View("~./Views/Testing/PlayerCommandPost.cshtml");
+            return View();
+        }
+    }
+}
